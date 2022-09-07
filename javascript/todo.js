932141cee2bb36ea70f2bf05f3998d56fd95284b
agiratech-sanjay <sanjay.k@agiratech.com>
2022-09-07 18:53:39 +0530

    third commit

--- javascript/todo.js
+++ javascript/todo.js
@@ -6,10 +6,12 @@ let filter = document.getElementById('filter');
 
 
 let arr = [];
+const months = ["1","2","3","4","5","6","7","8","9","10","11","12"];
+
 
 let date=new Date();
 let day = date.getDate();
-let month = date.getMonth();
+let month = months[date.getMonth()];
 let year = date.getFullYear();
 let fulldate = "Created On "+day + "-" +month+"-"+year;
 // console.log(fulldate);
@@ -78,16 +80,42 @@ addToDoButton.addEventListener('click', function(){
             }
 })
 
-var check = document.getElementById("all");
 
+
+let icon = document.getElementById("icon");
+let listcss = document.getElementById("listcss");
+// let i = document.getElementById("i");
+
+function show(){
+    listcss.classList.toggle("working");
+}
+function all1(){
+    icon.innerHTML = `<li id="all"><i class="fa-solid fa-filter"></i> ALL</li>`
+    listcss.className = "block"
+}
+
+function active1(){
+    icon.innerHTML = `<li id="active"><i class="fa-solid fa-filter"></i> ACTIVE</li>`
+    listcss.className = "block"
+}
+
+function complete1(){
+    icon.innerHTML = `<li id="complete"><i class="fa-solid fa-filter"></i> COMPLETE</li>`
+    listcss.className = "block"
+}
+
+// filter operation on all,active,complete.
+
+var check = document.getElementById("all");
 check.addEventListener("click",()=>{
     let filter = arr.filter((i)=>i.checkbox.checked === false || i.checkbox.checked === true);
     console.log(filter);
+    
+
 }
 )
 
 let cheeck = document.getElementById("active");
-
 cheeck.addEventListener("click",()=>{
     let filter = arr.filter((i)=>i.checkbox.checked === false);
     console.log(filter);
@@ -95,7 +123,6 @@ cheeck.addEventListener("click",()=>{
 )
 
 let che = document.getElementById("complete");
-
 che.addEventListener("click",()=>{
     let filter = arr.filter((i)=>i.checkbox.checked === true);
     console.log(filter);
